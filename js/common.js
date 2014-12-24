05d1c267d76ea873262e8dbe271743c5494bb890
buschidos <sergey.snjozyk@gmail.com>
2014-12-24 18:10:59 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,19 +1,59 @@
 head.ready(function() {
 
-    (function(){
-        var radioBtns = $('.figure').find('input[type="radio"]');
-        radioBtns.each(function() {
-            $(this).on('change', function() {
-                console.log($(this));
-                var radio = $(this),
-                    el = $(this).parents('.figure__item');
-                if (radio.is(':checked')) {
-                    el.addClass('is-active');
-                } else {
-                    el.removeClass('is-active');
-                }
+    (function() {
+        var leftElements = $('.figure__left').find('.figure__item');
+            rightElements = $('.figure__right').find('.figure__item');
+        function toggleActiveClass(elements) {
+            elements.on('click', function() {
+                elements.removeClass('is-active');
+                $(this).addClass('is-active');
             });
+        }
+        toggleActiveClass(leftElements);
+        toggleActiveClass(rightElements);
+    })();
+
+    (function() {
+        var phone          = $('.js-phone'),
+            phoneOffsetTop = phone.offset().top,
+            phoneHeight    = phone.height() - 95, //because phone image has bottom shadow
+            point,
+            topPos;
+
+        function calculateTopPos() {
+            topPos = $(window).height() / 2 - phoneHeight / 2;
+            point  = phoneOffsetTop + -topPos;
+        }
+
+        calculateTopPos();
+
+        $(window).on('resize', function() {
+            calculateTopPos();
+        });
+
+        $(document).on('scroll', function() {
+            if ( $(window).scrollTop() >= point ) {
+                phone.css({
+                    position   : 'fixed',
+                    top        : topPos
+                });
+            } else {
+                phone.css({
+                    position  : '',
+                    top       : ''
+                });
+            }
         });
     })();
 
+    function resizeFigure() {
+        var figure = $('.figure'),
+            scale;
+
+        figure.css({
+            '-webkit-transform': 'scale(' + scale + ')',
+                '-ms-transform': 'scale(' + scale + ')',
+                    'transform': 'scale(' + scale + ')',
+        });
+    }
 });
